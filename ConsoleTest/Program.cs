c626cbfae517d61a63e84185f3f758f7da3a122c
Burak Biçkioğlu <burakbickioglu@gmail.com>
2021-02-13 16:55:01 +0300

    Add project files.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Channels;
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager= new CarManager(new InMemoryCarDal());
+            
+            foreach (var c in carManager.GetAll())
+            {
+                Console.WriteLine(c.Id + " " + c.BrandId + " " + c.ColorId + " " + c.DailyPrice + " " + c.Description);
+            }
+
+            
+                
+        }
+    }
+}
